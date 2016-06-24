434a4eb0128dca319db3cd7714b552ec415b2ee7
Superman <superman08998@gmail.com>
2016-06-24 14:40:41 +0530

    feat(initial-architecture)

---
+++ non-theme-base/src/theme-base.module.ts
@@ -0,0 +1,13 @@
+const moduleName: string = 'non.theme-base';
+
+import * as angular from 'angular';
+
+import {IconConfig} from './base.icon.config';
+
+// Google Material Design Icons used from https://materialdesignicons.com/
+import './icons/mdi.svg';
+
+angular.module(moduleName, [])
+    .config(IconConfig);
+
+export default moduleName;
